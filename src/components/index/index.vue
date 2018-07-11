07293d68134bff22321e317872f5f17b97bee6e6
xiaoy <229469856@qq.com>
2018-07-11 21:22:12 +0800

    安装开发所用的包，增加首页组件，重置css

---
+++ src/components/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="index-wrap">
+        test
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        created() {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+<style>
+    .index-wrap{
+        width: 100%;
+        height: 100%;
+        background-color: #038387;
+    }
+</style>
