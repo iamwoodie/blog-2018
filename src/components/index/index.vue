91131561b16a31e186ac32f3c2114ff974416b40
xiaoy <229469856@qq.com>
2018-07-12 00:17:06 +0800

    修改index文件

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="index-wrap">
-        test
+        <ul class="shortcut-list">
+            <li class="list-item" v-for="(item,index) in shortCutList" :key="index">
+                <div class="">
+                    <img :src="item.icon" alt="">
+                </div>
+                <p>{{item.name}}</p>
+            </li>
+        </ul>
     </div>
 </template>
 <script>
     export default {
         data() {
             return {
-
+                shortCutList: [
+                    {
+                        name: '我的电脑',
+                        icon: ''
+                    }
+                ]
             }
         },
         created() {
@@ -18,10 +30,13 @@
         }
     }
 </script>
-<style>
+<style lang="less">
     .index-wrap{
         width: 100%;
         height: 100%;
         background-color: #038387;
+        .shortcut-list{
+            margin-bottom: 20px;
+        }
     }
 </style>
