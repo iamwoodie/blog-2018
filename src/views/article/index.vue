31e98fd8c8c5bb4515b6a7c17a1a8f5f4203e767
xiaoyong <229469856@qq.com>
2019-01-30 16:06:01 +0800

    微调文章样式

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -6,63 +6,128 @@
             </div>
         </div>
         <div class="right-bar">
-            <v-card></v-card>
-            <v-card></v-card>
+            <!-- <v-card></v-card>
+            <v-card></v-card> -->
         </div>
     </div>
 </template>
 
 <script>
 import vCard from '@/components/v_card/'
-import { getArticle } from '@/api/'
-import './markdown.css'
+import {
+	getArticle
+} from '@/api/'
 
 export default {
-    components: {
-        vCard
-    },
-    data() {
-        return{
-            article: {
+	components: {
+		vCard
+	},
+	data() {
+		return {
+			article: {
 
-            }
-        }
-    },
-    created() {
-        this.articleID = this.$route.query.articleID
-    },
-    mounted() {
-        this.getArticleHandle()
-    },
-    methods: {
-        getArticleHandle() {
-            getArticle(this.articleID).then(res => {
-                this.article = res;
-            }).catch(error => {
-                console.log(error)
-            })
-        }
-    }
+			}
+		}
+	},
+	created() {
+		this.articleID = this.$route.query.articleID
+	},
+	mounted() {
+		this.getArticleHandle()
+	},
+	methods: {
+		getArticleHandle() {
+			getArticle(this.articleID).then(res => {
+				this.article = res;
+			}).catch(error => {
+				console.log(error)
+			})
+		}
+	}
 }
 </script>
 
 <style lang="less">
-    @import './markdown.css';
-    .article-page{
-        display: flex;
-        justify-content: space-between;
+.article-page {
+    display: flex;
+    justify-content: space-between;
 
-        .article-content-wrap{
-            width: 735px;
-            padding: 15px;
+    .article-content-wrap {
+        width: 735px;
+        padding: 15px;
 
-            .article-content{
+        .article-content {
+            font-size: 12px;
+            color: #111111;
 
-            }
-        }
-
-        .right-bar{
-            width: 225px;
         }
     }
+
+    .right-bar {
+        width: 225px;
+    }
+}
+
+.markdown {
+    margin-bottom: 60px;
+    min-height: 700px;
+    h1,h2,h3,h4{
+      border-bottom: 1px solid #666;
+    }
+    h1{
+      font-size: 30px;
+      padding: 20px 0;
+      margin: 30px 0;
+    }
+    h2{
+      font-weight: 800;
+      font-size: 20px;
+      padding:20px 0;
+      margin-bottom: 30px;
+      border-color: #999;
+    }
+    h3{
+      font-size: 20px;
+      padding: 20px 0;
+      margin-bottom: 30px;
+      border-color: #999;
+    }
+    h4{
+        font-size: 20px;
+        padding: 20px 0;
+        margin-bottom: 30px;
+        border-color: #999;
+    }
+    p{
+      line-height: 30px;
+      font-size: 16px;
+      margin:10px 5px;
+      padding: 0 30px;
+    }
+    ul{
+      li{
+        p{
+          padding: 0 20px 0 60px;
+        }
+        p:nth-child(1){
+          font-weight: 800;
+          padding: 0 30px;
+        }
+      }
+    }
+    pre{
+      margin: 10px 30px;
+      padding: 0 70px;
+      line-height:20px;
+      white-space: pre-wrap;
+      white-space: -moz-pre-wrap;
+      white-space: -pre-wrap;
+      white-space: -o-pre-wrap;
+      word-wrap: break-word;
+    }
+    a{
+      color: #005599;
+      text-decoration:underline;
+    }
+}
 </style>
